382f662a6ca5dc7be558f241159427ed9c51a8a2
eusoubrunogusmao <brunomgusmao@gmail.com>
2025-04-07 18:08:22 -0300

    correcao exercicio 13

--- src/exrciciosbasicos02/Exercicio13.java
+++ src/exrciciosbasicos02/Exercicio13.java
@@ -2,10 +2,14 @@ package exrciciosbasicos02;
 
 import java.util.Scanner;
 
-public class Exerciocio13 {
+public class Exercicio13 {
 	public static void main(String[] args) {
 		
-	/*	13 Escreva um programa que leia um ano e diga se ele é bissexto ou não. Um ano é bissexto se ele for 		divisível por 4, exceto se ele for divisível por 100 e não por 400. 
+	/*	13 Escreva um programa que leia um ano e diga se ele é bissexto ou não. Um ano é bissexto se ele 	for divisível por 4, exceto se ele for divisível por 100 e não por 400. 
+
+			1. se o ano for divisivel por 4 ele é bissexto
+			2. se o ano for divisivel por 100 e nao for divisivel por 400 entao ele não é bissexto
+			   (se o ano nao for divisivel por 100 e for divisivel por 400 entao ele é bissexto
 
 		Ex:
 		2000 é bissexto. 
@@ -26,22 +30,26 @@ public class Exerciocio13 {
 						ano = scan.nextInt();
 			
 			
-				if (ano % 100 == 0 && ano % 400 != 0) {
+				if (ano % 4 == 0) {
+					if (ano % 100 == 0 && ano % 400 != 0) {
+						System.out.println("O ano " + ano + " não é bissexto.");
+					} else {
+						System.out.println("O ano " + ano + " é bissexto.");
+					}
+				} else {
 					System.out.println("O ano " + ano + " não é bissexto.");
-				} else if(ano % 4 == 0) {
-					System.out.println("O ano " + ano + " é bissexto.");
 				}
-				
 				scan.nextLine();
 				
 					System.out.println("Deseja informar outro ano? (s/n): ");
 					resposta = scan.nextLine().trim().toLowerCase();
-						while (!resposta.equalsIgnoreCase("s") && !resposta.equalsIgnoreCase("n")) {
+					
+						while (!resposta.equalsIgnoreCase("s") && 								!resposta.equalsIgnoreCase("n")) {
 							System.out.println("Entrada inválida! Digite 's' para sim ou 'n' para não: ");
 							resposta = scan.nextLine().trim().toLowerCase();
 						}
 			
-						continuar = (resposta.equalsIgnoreCase("s"));
+						continuar = resposta.equalsIgnoreCase("s");
 			
 		} while (continuar);
 			System.out.println("Programa encerrado!");
